7dccf9906931477c0d17e94a5804120746e40011
Ritwiz <madangopalsingh2803@gmail.com>
2024-09-01 12:06:12 +0530

    Minor changes

--- 02variables/main.go
+++ 02variables/main.go
@@ -4,19 +4,19 @@ import "fmt"
 
 func main() {
 	 
-	var username string = "Ritwiz"
+	var username string = "Ritwiz"//normal string
 	fmt.Println(username)
 	fmt.Printf("Variable is of type: %T \n",username)
 
-	var isLoggedIn bool = true
+	var isLoggedIn bool = true//bool value can be true or false
 	fmt.Println(isLoggedIn)
 	fmt.Printf("Variable is of type: %T \n",isLoggedIn)
 
-	var smallVal uint8 = 255
+	var smallVal uint8 = 255//uint 8's range is (0 to 2^8) similarly unit16's range is (0 to 2^16)
 	fmt.Println(smallVal)
 	fmt.Printf("Variable is of type: %T \n",smallVal)
 
-	var smallFloat float64 = 255.455548841622
+	var smallFloat float64 = 255.455548841622//more precision than float 32
 	fmt.Println(smallFloat)
 	fmt.Printf("Variable is of type: %T \n",smallFloat)
 
