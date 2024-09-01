87ff55a981e28fa7dae80c19f527f2657522b308
Ritwiz <madangopalsingh2803@gmail.com>
2024-09-01 12:03:56 +0530

    Basic variables

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,7 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	 
+	var username string = "Ritwiz"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n",username)
 
-	fmt.Println("Variables")
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n",isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n",smallVal)
+
+	var smallFloat float64 = 255.455548841622
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n",smallFloat)
 
 }
