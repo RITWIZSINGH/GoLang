d15334e28e9b6f56671f653b9d6d9486f66cdfbf
Ritwiz <madangopalsingh2803@gmail.com>
2024-09-05 00:43:32 +0530

    New folder mymaths created

---
+++ 05mymaths/go.mod
@@ -0,0 +1,3 @@
+module mymaths
+
+go 1.23.0
---
+++ 05mymaths/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var myNumberOne int = 2 // this is of type int
+	var myNumberTwo float64 = 3.5 // this is of type float
+
+	fmt.Println("The sum is: ", myNumberOne + int(myNumberTwo)) // 
+
+}
