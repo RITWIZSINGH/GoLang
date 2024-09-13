a22f10bcaa56902808a60a68b2ef3c66c8b0917b
Ritwiz <madangopalsingh2803@gmail.com>
2024-09-13 22:30:11 +0530

    minor change

--- 07mypointers/main.go
+++ 07mypointers/main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello Pointers")
+	fmt.Println("Welcome to the class on pointers")
 
 	var ptr *int // this is the type of declaration for pointers where u r creating a pointer in GO. It says ptr is a pointer to memory address which stores integer value. *string etc is also valid.
 
 	fmt.Println("Value of the pointer is", ptr) 
-
+	fmt.Println("****************************************************************************************************")
 	myNumber := 23
 
 	var myptr = &myNumber // Here notice that '*' sign is not used but instead '&' is used to reference to the var myNumber
