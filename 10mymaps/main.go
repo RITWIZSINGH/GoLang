f4a630ca9191d65ff2b22d12246db809976c2de6
Ritwiz <madangopalsingh2803@gmail.com>
2024-11-03 16:42:28 +0530

    mymaps completed

--- 10mymaps/main.go
+++ 10mymaps/main.go
@@ -1,9 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
 	fmt.Println("Welcome to Go Maps")
+
+	//This is the declaration for a map in 
+	languages := make(map[string]string)
+
+	//This is the initialization Eg: ["JS"], ["RB"] they are the keys
+	languages["JS"] = "Javascript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+
+
+	fmt.Println("This is the languages map", languages)
+	fmt.Println("JS shorts for" , languages["JS"])
+
+	//THis is how you can delete a key value pair from a map
+	delete(languages,"RB")
+
+	fmt.Println("This is the languages map", languages)
+
+	//Loops 
+
+	for key,value := range languages {
+		fmt.Printf("For key %v, the value is %v\n", key,value)
+	}
+
+
+
+
+	
 
 }
