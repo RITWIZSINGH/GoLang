9db70e3bd546c7b5b5848c2789f75eeb4cd4f522
Ritwiz <madangopalsingh2803@gmail.com>
2024-12-08 00:36:59 +0530

    if else complete

--- 12ifelse/main.go
+++ 12ifelse/main.go
@@ -5,5 +5,32 @@ import "fmt"
 func main() {
 
 	fmt.Println("Welcome to ifElse")
+
+	loginCount := 10
+	var result string
+
+	//the '{' should be in the same line
+	if loginCount > 10 {
+		result = "Regular user"
+	} else if loginCount < 10{
+		result = "Less than 10 count"
+	} else {
+		result = "Exactly 10 count"
+	}
+
+	if 9%2 == 0 {
+		fmt.Println("Number is even")
+	}else {
+		fmt.Println("number is ODD")
+	}
+
+	//Special syntax you can initiazlize variables on the go too
+	if num:= 3;num<10 {
+		fmt.Print("Number is less than 10")
+	}else {
+		fmt.Print("Number is NOT less than 10")
+	}
+
+	fmt.Println(result)
 	
 }
